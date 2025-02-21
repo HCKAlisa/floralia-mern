e33757276cb1894924079cdeeffa32c4a97773a4
HCKAlisa <hckalisa@gmail.com>
2025-02-20 23:04:54 -0800

    Initial commit

---
+++ client/src/components/link.tsx
@@ -0,0 +1,22 @@
+import AnchorLink from "react-anchor-link-smooth-scroll";
+import { SelectedPage } from "../shared/types";
+
+type Props = {
+    page: string;
+    selectedPage: SelectedPage,
+    setSelectedPage: (value:SelectedPage) => void
+}
+
+const Link = ({page, selectedPage, setSelectedPage}: Props) => {
+    const lowerCasePageName = page.toLocaleLowerCase().replace(/ /g, "") as SelectedPage
+    return (
+        <AnchorLink
+            href={`#${lowerCasePageName}`}
+            className={`${selectedPage === lowerCasePageName ? "bg-yellow-100 px-4 rounded-full" : ""} text-xl transition duration-500 hover:bg-orange-100 px-4 rounded-full`}
+            onClick={() => setSelectedPage(lowerCasePageName)}>
+            {page}
+        </AnchorLink>
+    )
+}
+
+export default Link
---
+++ client/src/shared/types.ts
@@ -0,0 +1,6 @@
+export enum SelectedPage {
+    Home = "games",
+    Team = "team",
+    FAQ = "faq",
+    Contact = "contact"
+}
