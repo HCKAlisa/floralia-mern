3a32905d9d3625616f95b94bcfc7543944baf2ae
HCKAlisa <hckalisa@gmail.com>
2025-03-21 19:16:06 -0700

    trying to fix deploy client not showing issue

--- server/index.ts
+++ server/index.ts
@@ -44,7 +44,7 @@ const __dirname = path.resolve();
 app.use("/api/user", (userRoutes));
 app.use("/api/auth", (authRoutes));
 
-app.use(express.static(path.join(__dirname, 'client/dist')));
+app.use(express.static(path.join(__dirname, '../../client/dist')));
 app.get('*', (_req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 })
