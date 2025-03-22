038ba5571174c7a7c40f4db7ff832b282941bb99
HCKAlisa <hckalisa@gmail.com>
2025-03-21 19:29:08 -0700

    trying to fix deploy client not showing issue

--- server/index.ts
+++ server/index.ts
@@ -35,6 +35,8 @@ mongoose.connect(uri, {
 });
 
 const __dirname = path.resolve();
+const __parentDir = path.resolve(__dirname, '..');
+//console.log(__parentDir)
 
 
 // app.get('/', (_req: Request, res: Response) => {
@@ -44,10 +46,10 @@ const __dirname = path.resolve();
 app.use("/api/user", (userRoutes));
 app.use("/api/auth", (authRoutes));
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
+app.use(express.static(path.join(__parentDir, '/client/dist')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+    res.sendFile(path.join(__parentDir, 'client', 'dist', 'index.html'));
 });
 
 //error middleware
